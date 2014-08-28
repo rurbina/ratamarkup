a20d9008dc6c695181debfdd74d0965f3e75ebe2
Ricardo Rat Urbina <rat@sdf.org>
2014-08-28 13:59:13 -0500

    Implementé las listas, con soporte para listas indentadas y para diferentes tipos de indicadores: -, + y * están soportados para listas sin orden; #, 1. 1) están soportados para listas ordenadas; : y los de listas sin orden están soportados para listas de definiciones. Las listas de definiciones se la rifan para soportar la sintaxis:  - term :: def1  - def2  - def3 que de todas formas no es óptima, pero bueeeeeeh.
    También agregué soporte para encabezados que empiecen con *. Esto es para compatibilidad con el modo Org de Emacs. Sólo funcionan si esa misma línea termina con un asterisco o con un signo de igual, es decir:
    * esto es un encabezado *
    * y esto es un item de lista
    
    Si lo indentas entonces es lista aunque termine en asterisco.

--- ratamarkup.rkt
+++ ratamarkup.rkt
@@ -1,11 +1,17 @@
 #lang racket/base
 
 (provide ratamarkup
-         (rename-out [inline-processors ratamarkup-inline-processors]
-                     [link-callback     ratamarkup-link-callback]))
+         (rename-out [inline-processors     ratamarkup-inline-processors]
+                     [link-callback         ratamarkup-link-callback]
+                     [section-processors    ratamarkup-section-processors]
+                     [add-section-processor ratamarkup-add-section-processor]))
+
 (require racket/string
          racket/list
+         racket/set
          xml)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define link-callback (lambda (link)
                         (let ([m (flatten (regexp-match* "^\\[\\[(.*)?(?<!\\\\)\\|(.*?)(?<!\\\\)\\|(.*?)]]$"
@@ -18,12 +24,13 @@
                                       (string-append " " (regexp-replace* #px"&quot;" (second m) "\"")))
                                   (third m)))))
 
-
 (define inline-processors
   '([#px"&" "\\&amp;"]
     [#px"<" "\\&lt;"]
     [#px">" "\\&gt;"]
     [#px"\"" "\\&quot;"]
+    [#px"\\\\\\\\" "\\&#92;"]
+    [#px"(?<!\\\\)(`+)\\s*(.*?)\\s*(?<!\\\\)\\1" "<code>\\2</code>"]
     [#px"(?<!')(?<!\\\\)'{4,5}([^']+)(?<!\\\\)'{4,5}(?!')" "<b><i>\\1</i></b>"]
     [#px"(?<=\\b)(?<!\\\\)__(.*?)(?<!\\\\)__(?=$|\\b)" "<u>\\1</u>"]
     [#px"(?<=\\b)(?<!\\\\)_-(.*?)(?<!\\\\)-_(?=$|\\b)" "<s>\\1</s>"]
@@ -33,30 +40,51 @@
     [#px"(?<!\\\\)\\[\\[([^]|]+|\\\\\\]|\\\\\\|)\\|([^]|]+|\\\\\\])\\]\\]" "[[\\1||\\2]]"]
     [#px"(?<!')'{3}([^']+)'{3}(?!')" "<b>\\1</b>"]
     [#px"(?<!')'{2}([^']+)'{2}(?!')" "<i>\\1</i>"]
-    [#px"\\\\(['{}|_^]|\\[|\\]|&quot;)" "\\1"]))
+    [#px"\\\\(['{}|_^`]|\\[|\\]|&quot;)" "\\1"]
+    [#px"^\\s+|\\s+$" ""]))
 
-(define (ratamarkup-inline text)
-  (string-join 
-   (map (lambda (line)
-          (let ([stage1 (regexp-replaces line inline-processors)]
-                [links null])
-            (set! links (regexp-match* #px"\\[\\[.*?(?<!\\\\)\\]\\]" stage1))
-            (for ([link links]) (set! stage1 (string-replace stage1 link (link-callback link))))
-            stage1))
-        (string-split text "\n"))
-   "\n"))
-  
-(define (compress items)
-  (filter (lambda (x) (not (empty? (second x))))
-                 (let ([out (list)] [acc '()] [type 'void])
-                   (for ([item items])
-                     (cond [(eq? type (car item)) (set! acc (append acc (list (cdr item))))]
-                           [else (set! out (append out (list (list type acc))))
-                                 (set! acc (list (cdr item)))
-                                 (set! type (car item))]))
-                   (unless (empty? acc)
-                     (set! out (append out (list (list type acc)))))
-                   out)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define paragraph-types
+  (hash
+   #px"^[|]" 'table
+   #px"^=|^[*=].*[*=]$" 'heading
+   #px"^!" 'comment
+   #px"^>" 'bquote
+   #px"^(\\s*[+:-]|\\s+[*]).*?[:]{2}" 'dlist
+   #px"^(\\s*[+-]|\\s+[*]).*" 'ulist
+   #px"^\\s*\\d+[.()]|^\\s*#" 'olist
+   #px"^\\s{2,}\\S+" 'continue-list
+   #px"^$" 'void))
+
+; this is pretty much the core of ratamarkup
+; this is where we actually turn text into html
+(define (process-std text
+                     #:options [options (hash)]
+                     #:tokens [tokens (list)])
+  (let ([lines (list)] [paragraphs '()])
+    (set! lines (regexp-split #px"\n" text))
+    (map
+     (lambda (line)
+       (let ([type 'para])
+         (for ([pattern (hash-keys paragraph-types)]
+               #:unless (and (not (empty? paragraphs))
+                             (eq? (hash-ref paragraph-types pattern) 'continue-list)
+                             (not (set-member? '(dlist ulist olist) (car (last paragraphs)))))
+               #:break (not (eq? type 'para)))
+           (when (regexp-match? pattern line)
+             (set! type (hash-ref paragraph-types pattern))))
+         (when (eq? type 'continue-list) (set! type (car (last paragraphs))))
+         (when (and (eq? type 'ulist) (eq? (car (last paragraphs)) 'dlist)) (set! type 'dlist))
+         (set! paragraphs (if (empty? paragraphs)
+                              (list (cons type line))
+                              (append paragraphs (list (cons type line)))))))
+     lines)
+    (string-join
+     (map (lambda (p)
+            ((hash-ref section-processors (first p)) (string-join (second p) "\n")))
+          (compress paragraphs))
+     "")))
 
 (define (process-table text
                        #:options [options (hash)]
@@ -73,86 +101,147 @@
          rows)
     output))
 
-(define paragraph-types
-  (hash
-   #px"^[|]" 'table
-   #px"^="   'heading
-   #px"^$"   'void))
+(define (process-para text)
+  (format "<p>\n\t~a\n</p>\n\n" (string-replace (ratamarkup-inline text) "\n" "\n\t" )))
 
-; this is pretty much the core of ratamarkup
-; this is where we actually turn text into html
-(define (process-std text
-                     #:options [options (hash)]
-                     #:tokens [tokens (list)])
-  (let ([lines (list)] [paragraphs '()])
-    (set! lines (regexp-split #px"\n" text))
-    (map
-     (lambda (line)
-       (let ([type 'para])
-         (for ([pattern (hash-keys paragraph-types)])
-           #:break (not (eq? type 'para))
-           (when (regexp-match? pattern line)
-             (set! type (hash-ref paragraph-types pattern))))
-         (set! paragraphs (if (empty? paragraphs)
-                              (list (cons type line))
-                              (append paragraphs (list (cons type line)))))))
-     lines)
+(define (process-dlist text)
+  (let ([items (for/list ([item (process-list-itemize text)])
+                 (if (list? item)
+                     (format "\t<dd>~v</dd>\n" item)
+                     (if (regexp-match? #px"[:]{2}" item)
+                         (format "\t<dt>~a</dt><dd>~a</dd>\n"
+                                 (ratamarkup-inline
+                                  (first (regexp-match #px"^.*?(?=\\s*?[:]{2})" item)))
+                                 (ratamarkup-inline
+                                  (first (regexp-match #px"(?<=[:]{2}).*" item))))
+                         (format "\t<dd>~a</dd>\n" (xexpr->string item)))))])
     (string-join
-     (map (lambda (p)
-            ((hash-ref section-processors (first p)) (string-join (second p) "\n")))
-          (compress paragraphs))
-     "")))
+     items
+     ""
+     #:before-first "<dl>\n"
+     #:after-last "</dl>\n\n")))
+
+(define (process-olist text)
+  (let ([items (for/list ([item (process-list-itemize text)])
+                 (if (list? item)
+                     (format "\t<li>~v</li>\n" item)
+                     (format "\t<li>~a</li>\n" (ratamarkup-inline item))))])
+    (string-join
+     items
+     ""
+     #:before-first "<ol>\n"
+     #:after-last "</ol>\n\n")))
+
+(define (process-ulist text)
+  (let ([items (for/list ([item (process-list-itemize text)])
+                 (if (list? item)
+                     (format "\t<li>~v</li>\n" item)
+                     (format "\t<li>~a</li>\n" (ratamarkup-inline item))))])
+    (string-join
+     items
+     ""
+     #:before-first "<ul>\n"
+     #:after-last "</ul>\n\n")))
+
+(define (process-list-itemize text)
+  (let ([leadup (flatten (regexp-match* #px"(?s:^\\s*([+:*-]|\\d+[.)]|#))" text #:match-select car))]
+        [leader ""]
+        [leader-regexp ""])
+    (set! leader (first leadup))
+    (if (regexp-match? #px"^(\\s*)\\d+([.()])" leader)
+      (set! leader-regexp (regexp-replace #px"^(\\s*)\\d+([.()])" leader "^\\1\\\\d+\\2"))
+      (set! leader-regexp (string-append "^" leader)))
+    (set! leader-regexp (regexp-replace* #px"((?<!\\\\d)[*+:().])" leader-regexp "\\\\\\1"))
+    (set! leader-regexp (pregexp leader-regexp))
+    (for/list ([line (string-split text "\n")])
+      (if (regexp-match leader-regexp line)
+          (regexp-replace #px"^\\s*" (substring line (string-length leader)) "")
+          (list line)))))
 
 (define section-processors
-  (hash 
-   'table   process-table
-   'std     process-std
-   'para    (lambda (text) (format "<p>\n\t~a\n</p>\n" (string-replace (ratamarkup-inline text) "\n" "\n\t" )))
-   'void    (lambda (text) "")
-   'heading (lambda (text)
-              (string-join
-               (map (lambda (line)
-                      (let ([lvl (string-length (car (regexp-match #px"(?s:^=+)" line)))])
-                        (set! lvl (min 6 lvl))
-                        (string-join
-                         (list (format "<h~a>" lvl)
-                               (string-replace
-                                (xexpr->string (regexp-replace #px"(?s:^=+\\s*(.*?)\\s*=*$)" line "\\1"))
-                                 "\n" "")
-                               (format "</h~a>" lvl))
-                         "")))
-                    (string-split text "\n"))
-              "\n"
-              #:after-last "\n\n"))
-   'default (lambda (text) (format "<!-- no section -->\n~a" text))))
+  (make-hash 
+   `([table   . ,process-table]
+     [std     . ,process-std]
+     [para    . ,process-para]
+     [bquote  .,(lambda (text) (format "<blockquote>~a</blockquote>\n\n"
+                                  (regexp-replace #px"(?m:^)" (process-std (regexp-replace* #px"(?m:^> ?)" text "")) "\t")))]
+     [dlist   . ,process-dlist]
+     [olist   . ,process-olist]
+     [ulist   . ,process-ulist]
+     [comment . ,(lambda (text) (format "<!-- ~a -->\n" (regexp-replace #px"(?m:^!)" text "")))]
+     [void    . ,(lambda (text) "")]
+     [heading . ,(lambda (text)
+                (string-join
+                 (map (lambda (line)
+                        (let ([lvl (string-length (car (regexp-match #px"(?s:^[*=]+)" line)))])
+                          (set! lvl (min 6 lvl))
+                          (string-join
+                           (list (format "<h~a>" lvl)
+                                 (string-replace
+                                  (xexpr->string (regexp-replace #px"(?s:^[*=]+\\s*(.*?)\\s*[*=]*$)" line "\\1"))
+                                  "\n" "")
+                                 (format "</h~a>" lvl))
+                           "")))
+                      (string-split text "\n"))
+                 "\n"
+                 #:after-last "\n\n"))]
+     [default . ,(lambda (text) (format "<!-- no section -->\n~a" text))])))
+
+(define (add-section-processor key value)
+  (hash-set! section-processors key value))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (compress items)
+  (filter (lambda (x) (not (empty? (second x))))
+                 (let ([out (list)] [acc '()] [type 'void])
+                   (for ([item items])
+                     (cond [(eq? type (car item)) (set! acc (append acc (list (cdr item))))]
+                           [else (set! out (append out (list (list type acc))))
+                                 (set! acc (list (cdr item)))
+                                 (set! type (car item))]))
+                   (unless (empty? acc)
+                     (set! out (append out (list (list type acc)))))
+                   out)))
+
+(define (ratamarkup-inline text)
+  (string-join 
+   (map (lambda (line)
+          (let ([stage1 (regexp-replaces line inline-processors)]
+                [links null])
+            (set! links (regexp-match* #px"\\[\\[.*?(?<!\\\\)\\]\\]" stage1))
+            (for ([link links]) (set! stage1 (string-replace stage1 link (link-callback link))))
+            stage1))
+        (string-split text "\n"))
+   "\n"))
+  
 
 ; parse tokens
 ; valid tokens are key="value value", key=value and key
 (define (parse-tokens [token-string string?])
-  (let ([tokens null])
+  (let ([tokens null] [parsed-tokens '()])
     (set! tokens (flatten
                   (regexp-match*
                    #px"(\\S+=\".*?(?<!\\\\)\"|\\S+=\\S+|\\S+)"
                    token-string
                    #:match-select cdr)))
-    (map (lambda (token)
-           (cond
-            [(regexp-match? "=\"" token) (set!-values 
-                                          (token)
-                                          (flatten
-                                           (regexp-match*
-                                            #px"(.*)?=\"(.*)\"$"
-                                            token
-                                            #:match-select cdr)))]
-            [(regexp-match? "=" token) (set!-values
-                                        (token)
-                                        (flatten
-                                         (regexp-match*
-                                          #px"(.*)?=(.*)$"
-                                          token
-                                          #:match-select cdr)))]))
-         tokens)
-    tokens))
+    (set! parsed-tokens
+          (for/list ([token tokens])
+            (cond
+             [(regexp-match? "=\"" token)
+              (set!-values (token)
+                           (flatten (regexp-match* #px"(.*)?=\"(.*)\"$" token #:match-select cdr)))
+              (set! token (cons (string->symbol (first token)) (second token)))
+              token]
+             [(regexp-match? "=" token) 
+              (set!-values (token)
+                           (flatten (regexp-match* #px"(.*)?=(.*)$" token #:match-select cdr)))
+              (set! token (cons (string->symbol (first token)) (second token)))
+              token]
+             [else 
+              (set! token (cons (string->symbol token) #t))
+              token])))
+    parsed-tokens))
 
 (struct rm-section (type tokens body))
 
@@ -172,7 +261,7 @@
             (set! body (second matches))
             (set! tokens (parse-tokens head))
             (unless (empty? tokens)
-              (set! type (string->symbol (car tokens)))
+              (set! type (caar tokens))
               (set! tokens (cdr tokens)))
             (set! sections (append sections (list (rm-section type tokens body)))))
           (set! sections (append sections (list (rm-section default '() sbody)))))
